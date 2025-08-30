3f17a2fac859118c6c7582227188c6e0dcb2e3b2
alexey.rand <alexey.rand@yandex.ru>
2025-08-30 23:48:32 +0300

    develop

--- src/main/java/ru/alexeyrand/whoistobuybase/config/BaseAutoConfiguration.java
+++ src/main/java/ru/alexeyrand/whoistobuybase/config/BaseAutoConfiguration.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import ru.alexeyrand.whoistobuybase.repositories.UserRepository;
 import ru.alexeyrand.whoistobuybase.services.UserService;
-//
-//@Configuration
-//@EnableJpaRepositories(basePackages = {"ru.alexeyrand.whoistobuybase.repositories"})
-//public class BaseAutoConfiguration {
-//
-//    @Bean
-//    public UserService userService(UserRepository userRepository) {
-//        return new UserService(userRepository);
-//    }
-//
-//}
+
+@Configuration
+@EnableJpaRepositories(basePackages = {"ru.alexeyrand.whoistobuybase.repositories"})
+public class BaseAutoConfiguration {
+
+    @Bean
+    public UserService userService(UserRepository userRepository) {
+        return new UserService(userRepository);
+    }
+
+}
